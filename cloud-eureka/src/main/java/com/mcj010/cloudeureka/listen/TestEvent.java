ca9eb295cb82ab858092d4afea2dcfbe2ee2b561
mcj010 <329392494@qq.com>
2020-09-25 22:05:58 +0800

    开启相互注册和改变hostname，解决unavailable-replicas 问题。

--- cloud-eureka/src/main/java/com/mcj010/cloudeureka/listen/TestEvent.java
+++ cloud-eureka/src/main/java/com/mcj010/cloudeureka/listen/TestEvent.java
@@ -1,7 +1,11 @@
 package com.mcj010.cloudeureka.listen;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Component
 public class TestEvent {
@@ -9,5 +13,12 @@ public class TestEvent {
 
         //发邮件 短信
         System.out.println("下线"+event.getServerId());
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+
+//        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2,
+//                new ThreadFactoryBuilder()
+//                        .setNameFormat("DiscoveryClient-%d")
+//                        .setDaemon(true)
+//                        .build());
     }
 }
