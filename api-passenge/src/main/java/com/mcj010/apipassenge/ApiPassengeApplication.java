e639491b2ded81e41fe9e6561b16c8d9a88c93e7
mcj010 <329392494@qq.com>
2020-09-25 22:06:58 +0800

    写了一个GUAVA cache 的例子

--- api-passenge/src/main/java/com/mcj010/apipassenge/ApiPassengeApplication.java
+++ api-passenge/src/main/java/com/mcj010/apipassenge/ApiPassengeApplication.java
@@ -1,13 +1,44 @@
 package com.mcj010.apipassenge;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import com.google.common.cache.CacheBuilder;
+        import com.google.common.cache.CacheLoader;
+        import com.google.common.cache.LoadingCache;
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.PathVariable;
+        import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+        import java.util.Random;
+        import java.util.concurrent.ExecutionException;
+        import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@RestController
 public class ApiPassengeApplication {
+
+    LoadingCache cache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.SECONDS).build(new CacheLoader<String, String>() {
+        @Override
+        public String load(String key) throws Exception {
+            return "Load:" + new Random().nextInt(100);
+        }
+    });
 
     public static void main(String[] args) {
         SpringApplication.run(ApiPassengeApplication.class, args);
     }
+
+    @PostMapping("/test-set/{id}")
+    public String testSet(@PathVariable String id) {
+        cache.put(id, "Set:" + new Random().nextInt(100));
+        return "success";
+    }
+
+    @GetMapping("/test-get/{id}")
+    public String testGet(@PathVariable String id) throws ExecutionException {
+        return cache.get(id).toString();
+    }
+
 
 }
