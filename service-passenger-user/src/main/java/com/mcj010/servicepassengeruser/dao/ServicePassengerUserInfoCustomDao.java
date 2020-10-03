63ee9151ab4e3df3e145f35038950f27a3823831
mcj010 <329392494@qq.com>
2020-10-03 21:16:24 +0800

    加入metadata 自定义version版本

--- service-passenger-user/src/main/java/com/mcj010/servicepassengeruser/dao/ServicePassengerUserInfoCustomDao.java
+++ service-passenger-user/src/main/java/com/mcj010/servicepassengeruser/dao/ServicePassengerUserInfoCustomDao.java
@@ -2,8 +2,10 @@ package com.mcj010.servicepassengeruser.dao;
 
 import com.mcj010.servicepassengeruser.entity.ServicePassengerUserInfo;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Service;
 
 @Mapper
+@Service
 public interface ServicePassengerUserInfoCustomDao extends ServicePassengerUserInfoDao{
 
     /**
